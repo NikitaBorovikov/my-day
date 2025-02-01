541afa32ae4afb6e02216d1f082397994c2c2954
NikitaBorovikov <nikitaborovikov23@yandex.ru>
2025-02-01 15:59:55 +0300

    added usecase logic for myDay

--- pkg/usecases/myDay.go
+++ pkg/usecases/myDay.go
@@ -1,6 +1,9 @@
 package usecases
 
-import "toDoApp/pkg/model"
+import (
+	"time"
+	"toDoApp/pkg/model"
+)
 
 type MyDayUseCase struct {
 	MyDayRepository model.MyDayRepository
@@ -13,5 +16,22 @@ func NewMyDayUseCase(myDayRepository model.MyDayRepository) *MyDayUseCase {
 }
 
 func (uc *MyDayUseCase) Get(userID int64, date string) (*model.MyDay, error) {
-	return nil, nil
+
+	formatedDate, err := formattingDate(date)
+	if err != nil {
+		return nil, err
+	}
+
+	myDay, err := uc.MyDayRepository.Get(userID, formatedDate)
+	return myDay, err
+}
+
+func formattingDate(inputDate string) (string, error) {
+	parseDate, err := time.Parse("01-02-2006", inputDate)
+	if err != nil {
+		return "", err
+	}
+
+	fornatedDate := parseDate.Format(time.RFC3339)
+	return fornatedDate, nil
 }
