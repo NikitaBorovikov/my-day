cc64e774d3cd2638db0a3bbee84d43dc920756e5
NikitaBorovikov <nikitaborovikov23@yandex.ru>
2025-02-01 16:39:32 +0300

    corrected myDay repository

--- pkg/repository/postgres/myDay.go
+++ pkg/repository/postgres/myDay.go
@@ -18,7 +18,7 @@ func NewMyDayRepository(db *sql.DB) model.MyDayRepository {
 }
 
 func (r *MyDayRepository) Get(userID int64, date string) (*model.MyDay, error) {
-	query := "SELECT task.title, task.description, task.is_important, events.name, events.description FROM task INNER JOIN events ON task.user_id = events.user_id WHERE task.user_id = $1 AND events.user_id = $1 AND task.due_date = '$2' AND events.appointed_date = '$2'"
+	query := "SELECT task.title, task.description, task.is_important, events.name, events.description FROM task INNER JOIN events ON task.user_id = events.user_id WHERE task.user_id = $1 AND events.user_id = $1 AND task.due_date = $2 AND events.appointed_date = $2"
 	rows, err := r.db.Query(query, userID, date)
 	if err != nil {
 		logrus.Info(err)
